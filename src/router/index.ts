e06c13a84febae49a7d1b6f9ad14b97d1529e1f4
Leon <236338364@qq.com>
2021-05-28 19:28:57 +0800

    music

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import {
   createRouter,
   createWebHashHistory,
+  createWebHistory,
   RouteRecordRaw
 } from 'vue-router'
 import Home from '@/views/home.vue'
 import Vuex from '@/views/vuex.vue'
+import Music from '@/views/music.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,11 +23,16 @@ const routes: Array<RouteRecordRaw> = [
     path: '/axios',
     name: 'Axios',
     component: () => import('@/views/axios.vue')
+  },
+  {
+    path: '/music',
+    name: 'Music',
+    component: Music
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ src/views/music.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>music</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from 'axios'
+
+export default defineComponent({
+  setup() {
+    
+    /**
+     * 歌手分类列表
+     * type  -1:全部 1:男歌手 2:女歌手 3:乐队
+     * area -1:全部 7华语 96欧美 8:日本 16韩国 0:其他
+     * limit 返回数量
+     * offset 偏移数量
+     * /artist/list
+     */
+    const getArtList = (type = -1, area = -1, limit = 20, offset = 1) => {
+      axios.get(`http://10.4.127.41:3000/artist/list/type=${type}&area=${area}&limit=${limit}&offset=${offset}&initial=b`).then(res => {
+        console.log(res)
+      })
+    }
+
+    getArtList(-1, -1, 30, 160)
+  },
+})
+</script>
